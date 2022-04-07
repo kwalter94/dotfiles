b37a3baa71db4629ee75cb7d295c81869953793a
Walter Kaunda <kwalter@magnus.localdomain>
2022-04-07 14:24:03 +0200

    omf: Add nerd-font flag for bobthefish theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,14 +9,26 @@ set -x PATH $PATH $ANDROID_HOME/tools
 set -x PATH $PATH $ANDROID_HOME/tools/bin
 set -x PATH $PATH $ANDROID_HOME/platform-tools
 
+function kubectl
+    minikube kubectl -- $argv
+end
+
 if [ $TILIX_ID ] || [ $VTE_VERSION ]
     bass 'source /etc/profile.d/vte-2.91.sh'
 end
 
-source ~/.asdf/asdf.fish
-bass 'source /home/kwalter/.asdf/installs/rust/1.59.0/env'
+if test -e ~/.asdf/asdf.fish
+    source ~/.asdf/asdf.fish
+end
+
+if test -e /home/kwalter/.asdf/installs/rust/1.59.0/env
+    bass 'source /home/kwalter/.asdf/installs/rust/1.59.0/env'
+end
 
 set -x GOPATH $HOME
-set -x DOTFILES_REPOSITORY ~/src/github.com/kwalter94/dotfiles
+# set -x DOTFILES_REPOSITORY ~/src/github.com/kwalter94/dotfiles
 set -x EDITOR (which nvim)
 
+if test (cat ~/.config/omf/theme) = 'bobthefish'
+    set -g theme_nerd_fonts yes
+end
