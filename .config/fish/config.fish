c4395989b507bd50dd0e2f52f25ea485b76f1a03
Walter Kaunda <walterkaunda@live.co.uk>
2022-04-18 17:33:20 +0200

    Fix initialisation of path env var
    Was getting modified on every invocation of fish. Thus in
    subshells, paths that should normally be given high priority
    were being demoted (eg python's virtual envs).

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,18 @@ function fish_greeting
     fortune | cowsay -f (ls /usr/share/cowsay/cows | shuf -n 1) | lolcat
 end
 
-set -x ANDROID_HOME $HOME/Android/Sdk
+if not contains $HOME/bin $PATH
+    set -gx PATH $HOME/bin $PATH 
+end
 
-set -x PATH $PATH $ANDROID_HOME/emulator
-set -x PATH $PATH $ANDROID_HOME/tools
-set -x PATH $PATH $ANDROID_HOME/tools/bin
-set -x PATH $PATH $ANDROID_HOME/platform-tools
+if not set -q ANDROID_HOME
+    set -gx ANDROID_HOME $HOME/Android/Sdk
+
+    set -gx PATH $ANDROID_HOME/emulator $PATH 
+    set -gx PATH $ANDROID_HOME/tools $PATH 
+    set -gx PATH $ANDROID_HOME/tools/bin $PATH 
+    set -gx PATH $ANDROID_HOME/platform-tools $PATH 
+end
 
 function kubectl
     minikube kubectl -- $argv
@@ -17,18 +23,14 @@ if [ $TILIX_ID ] || [ $VTE_VERSION ]
     bass 'source /etc/profile.d/vte-2.91.sh'
 end
 
-if test -e ~/.asdf/asdf.fish
+if not which asdf &> /dev/null; and [ -e ~/.asdf/asdf.fish ]
     source ~/.asdf/asdf.fish
 end
 
-if test -e /home/kwalter/.asdf/installs/rust/1.59.0/env
-    bass 'source /home/kwalter/.asdf/installs/rust/1.59.0/env'
-end
-
-set -x GOPATH $HOME
+set -gx GOPATH $HOME
 # set -x DOTFILES_REPOSITORY ~/src/github.com/kwalter94/dotfiles
-set -x EDITOR (which nvim)
+set -gx EDITOR (which nvim)
 
-if test (cat ~/.config/omf/theme) = 'bobthefish'
+if [ "(cat ~/.config/omf/theme)" = 'bobthefish' ]
     set -g theme_nerd_fonts yes
 end
